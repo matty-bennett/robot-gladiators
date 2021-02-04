511ef35393b6504b7f371a3d5d94fe16c4cd0cd1
matty-bennett <bennyot10@gmail.com>
2021-02-03 21:09:29 -0700

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,12 +4,7 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-console.log(playerName);
-console.log(playerHealth, playerAttack, playerMoney);
-console.log("You can put regular sentences in these.");
-
 var enemyNames = ["Roborto", "Amy Andriod", "Robo Trumble"];
-console.log(enemyNames);
 var enemyHealth = 50;
 var enemyAttack = 12;
 
@@ -20,7 +15,7 @@ var enemyAttack = 12;
 // "LOSE" - Player robot's health is zero or less
 
 // fight function
-var fight = function() {
+var fight = function(enemyName) {
   // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
 
@@ -32,20 +27,20 @@ var fight = function() {
     // remove enemy's health by subtracting the amount set in the playerAttack variable
     enemyHealth = enemyHealth - playerAttack;
     console.log(
-      playerName + " attacked " + /*enemyName*/ + ". " + /*enemyName*/ + " now has " + enemyHealth + " health remaining."
+      playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     );
 
     // check enemy's health
     if (enemyHealth <= 0) {
-      window.alert(/*enemyName*/ + " has died!");
+      window.alert(enemyName + " has died!");
     } else {
-      window.alert(/*enemyName*/ + " still has " + enemyHealth + " health left.");
+      window.alert(enemyName + " still has " + enemyHealth + " health left.");
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
     playerHealth = playerHealth - enemyAttack;
     console.log(
-      /*enemyName*/ + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+      enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
 
     // check player's health
@@ -76,4 +71,6 @@ var fight = function() {
 };
 
 // run fight function to start game
-// fight();
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
